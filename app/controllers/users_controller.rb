96a332f25435433847447927c7375bae71908af5
SuganoRyo <ryo22.0413@ezweb.ne.jp>
2021-09-25 22:25:31 +0900

    white space削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     end
   end
 
-  private 
+  private
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
