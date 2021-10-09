d9773e39c129805258f1386dc4d254569a77692c
SuganoRyo <ryo22.0413@ezweb.ne.jp>
2021-10-09 12:37:15 +0900

    carrierwave修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'searching-outlet-test'
+    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: 'AKIARYLFXZ3HQTHQKMTE',
       aws_secret_access_key: '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',
       region: 'ap-northeast-1'
     }
-
-    config.fog_directory  = 'searching-outlet-test'
-    config.cache_storage = :fog
-    config.fog_public = false
   end
 end
